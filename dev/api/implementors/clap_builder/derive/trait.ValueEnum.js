f57087c984bd8774462b23fdf4bfffcef3707793
datdenkikniet <datdenkikniet@users.noreply.github.com>
2023-04-09 23:22:47 +0000

    deploy: 2fb09d4a6da82a73572da7c7db132989b0dd4a49

---
+++ dev/api/implementors/clap_builder/derive/trait.ValueEnum.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {
+"clap":[],
+"clap_builder":[],
+"xtask":[["impl <a class=\"trait\" href=\"clap_builder/derive/trait.ValueEnum.html\" title=\"trait clap_builder::derive::ValueEnum\">ValueEnum</a> for <a class=\"enum\" href=\"xtask/argument_parsing/enum.Backends.html\" title=\"enum xtask::argument_parsing::Backends\">Backends</a>"],["impl <a class=\"trait\" href=\"clap_builder/derive/trait.ValueEnum.html\" title=\"trait clap_builder::derive::ValueEnum\">ValueEnum</a> for <a class=\"enum\" href=\"xtask/argument_parsing/enum.Package.html\" title=\"enum xtask::argument_parsing::Package\">Package</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
